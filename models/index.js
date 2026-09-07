53f8ff607b96bd5f2dbb8b9d6c3e054800bf676b
pmoilane <petteri.j.moilanen@student.oulu.fi>
2026-09-07 03:50:29 +0300

    sync models inside async function

--- models/index.js
+++ models/index.js
@@ -1,13 +1,17 @@
-const Blog = require('./blog')
 const User = require('./user')
+const Blog = require('./blog')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-Blog.sync({ alter: true })
-User.sync({ alter: true })
+const syncDb = async () => {
+  await User.sync({ alter: true })
+  await Blog.sync({ alter: true })
+}
+
+syncDb()
 
 module.exports = {
-  Blog,
   User,
+  Blog,
 }
